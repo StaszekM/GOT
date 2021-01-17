c7fe36e15461fbf86d172ee9601b57c2dca186c3
Stanisław Markowski <246741@student.pwr.edu.pl>
2021-01-17 23:34:58 +0100

    Add Bootstrap icons, initial Path controller and two mock views

---
+++ GOT/Controllers/PathController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using GOT.ViewModels;
+
+namespace GOT.Controllers {
+    public class PathController : Controller {
+        public IActionResult Index() {
+            //TODO replace with proper access to database
+            List<PathViewModel> result = new List<PathViewModel>();
+            result.Add(new PathViewModel { Id = 1, Area = "Sudety", Name = "Trasa testowa", CreationDate = new DateTime(2020, 11, 1) });
+            //end TODO
+            return View(result);
+        }
+
+        public IActionResult Create() {
+            return View();
+        }
+    }
+}
---
+++ GOT/ViewModels/PathViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace GOT.ViewModels {
+    //TODO replace points with proper representation instead of strings
+    public class PathViewModel {
+
+        public int Id { get; set; }
+        [Display(Name = "Nazwa")]
+        public string Name { get; set; }
+        [Display(Name = "Obszar")]
+        public string Area { get; set; }
+        [Display(Name = "Data utworzenia")]
+        public DateTime CreationDate { get; set; }
+        [Display(Name = "Punkt A")]
+        public string PointA { get; set; }
+        [Display(Name = "Punkt B")]
+        public string PointB { get; set; }
+        [Display(Name = "Suma wejść A-B")]
+        public int ElevationAB { get; set; }
+        [Display(Name = "Długość A-B")]
+        public int DistanceAB { get; set; }
+        [Display(Name = "Suma wejść B-A")]
+        public int ElevationBA { get; set; }
+        [Display(Name = "Długość B-A")]
+        public int DistanceBA { get; set; }
+        [Display(Name = "Opis")]
+        public string Description { get; set; }
+        
+    }
+}
