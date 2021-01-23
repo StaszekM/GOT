1e12f9be6a96b3de45de51d5e1a328d4d8241744
Stanisław Markowski <246741@student.pwr.edu.pl>
2021-01-23 13:09:10 +0100

    Add trip details view

--- GOT/Controllers/TripController.cs
+++ GOT/Controllers/TripController.cs
@@ -2,15 +2,29 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
+using System.Threading.Tasks;
 using GOT.ViewModels;
 
 namespace GOT.Controllers {
     public class TripController : Controller {
-        public IActionResult Index() {
+        public async Task<IActionResult> Index() {
             //TODO replace with proper access to database
             List<TripViewModel> result = new List<TripViewModel>();
             result.Add(new TripViewModel { Id = 1, Areas = new string[2] { "Sudety", "Beskidy" }, StartDate = new DateTime(2020, 11, 1), EndDate = new DateTime(2020, 11, 1), Score = 8 });
+            //end TODO
+            return View(result);
+        }
+
+        public async Task<IActionResult> Details(int? id) {
+            //TODO replace with proper access to database
+            TripViewModel result = new TripViewModel {
+                Id = 1,
+                Areas = new string[2] { "Sudety", "Beskidy" },
+                StartDate = new DateTime(2020, 11, 1),
+                EndDate = new DateTime(2020, 11, 1),
+                Score = 8,
+                PointNames = new string[2] { "Punkt 1", "Punkt 2" }
+            };
             //end TODO
             return View(result);
         }
