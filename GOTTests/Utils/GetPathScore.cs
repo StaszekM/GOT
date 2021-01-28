6dbf1acf257ea16709ebc5dd984941c467bb048a
Stanisław Markowski <246741@student.pwr.edu.pl>
2021-01-28 21:30:10 +0100

    Add unit tests for two utils

---
+++ GOTTests/Utils/GetPathScore.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using GOT.Models;
+
+namespace GOTTests.Utils {
+    public class GetPathScore {
+        [Fact]
+        public void EmptyPathReturnsZero() {
+            Path path = new Path();
+            bool isFromAToB = true;
+
+            int resultAB = GOT.Models.Utils.GetPathScore(path, isFromAToB);
+            int resultBA = GOT.Models.Utils.GetPathScore(path, !isFromAToB);
+
+            Assert.Equal(0, resultAB);
+            Assert.Equal(0, resultBA);
+        }
+
+        [Fact]
+        public void NormalPathReturnsRightNumber() {
+            Path path = new Path { ElevationAB = 200, DistanceAB = 1000, ElevationBA = 0, DistanceBA = 1000 };
+            bool isFromAToB = true;
+
+            int resultAB = GOT.Models.Utils.GetPathScore(path, isFromAToB);
+            int resultBA = GOT.Models.Utils.GetPathScore(path, !isFromAToB);
+
+            Assert.Equal(3, resultAB);
+            Assert.Equal(1, resultBA);
+        }
+
+        [Fact]
+        public void MethodProperlyRoundsScore() {
+            Path path = new Path { ElevationAB = 250, DistanceAB = 1500, ElevationBA = 251, DistanceBA = 1501 };
+            bool isFromAToB = true;
+
+            int resultAB = GOT.Models.Utils.GetPathScore(path, isFromAToB);
+            int resultBA = GOT.Models.Utils.GetPathScore(path, !isFromAToB);
+
+            Assert.Equal(3, resultAB);
+            Assert.Equal(5, resultBA);
+        }
+    }
+}
---
+++ GOTTests/Utils/GetTotalTripDistance.cs
@@ -0,0 +1,54 @@
+﻿using Xunit;
+using GOT.Models;
+
+namespace GOTTests.Utils {
+    public class GetTotalTripDistance {
+        [Fact]
+        public void EmptyTripReturnsZero() {
+            Trip trip = new Trip();
+
+            int result = GOT.Models.Utils.GetTotalTripDistance(trip);
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void NormalTripReturnsRightNumber() {
+            Trip trip = new Trip() {
+                PathTrips = new PathTrip[3] {
+                    new PathTrip {
+                        Path = new Path {
+                            ElevationAB = 100,
+                            DistanceAB = 1000,
+                            ElevationBA = 0,
+                            DistanceBA = 1000
+                        },
+                        IsFromAToB = true
+                    },
+                    new PathTrip {
+                        Path = new Path {
+                            ElevationAB = 200,
+                            DistanceAB = 3000,
+                            ElevationBA = 0,
+                            DistanceBA = 2000
+                        },
+                        IsFromAToB = false
+                    },
+                    new PathTrip {
+                        Path = new Path {
+                            ElevationAB = 0,
+                            DistanceAB = 4000,
+                            ElevationBA = 100,
+                            DistanceBA = 4000
+                        },
+                        IsFromAToB = true
+                    }
+                }
+            };
+
+            int result = GOT.Models.Utils.GetTotalTripDistance(trip);
+
+            Assert.Equal(7000, result);
+        }
+    }
+}
